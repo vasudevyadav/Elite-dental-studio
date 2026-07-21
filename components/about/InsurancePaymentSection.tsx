1d91636d5161d0c93fe1168d2ff8b00976e2a079
dentistcharan <dentist.charan@gmail.com>
2026-07-21 11:19:06 +0530

    new changes add

--- components/about/InsurancePaymentSection.tsx
+++ components/about/InsurancePaymentSection.tsx
@@ -9,7 +9,7 @@ export default function InsurancePaymentSection() {
           <h2 className="text-2xl font-black uppercase text-[#29666b] sm:text-3xl">Insurance and Payment</h2>
           <p className="mt-5 text-sm lg:text-base leading-6 text-[#58696a]">We Accept all modes of payments including Cash, Credit/Debit Cards, Google Pay, UPI Payment, Razorpay Payment Gateway and EMI options are also available.</p>
           <p className="mt-4 text-sm lg:text-base leading-6 text-[#58696a]">Bills will be provided for Insurance Claims to be done in GCC countries.</p>
-          <Image src="/about/payment.png" alt="Accepted payment methods" width={950} height={450} className="mt-7 w-full !h-[450px] rounded-[18px] bg-[#e8f8f6] p-3 shadow-[0_8px_20px_rgba(25,90,93,.12)]" />
+          <Image src="/about/payment.png" alt="Accepted payment methods" width={950} height={450} className="mt-7 w-full !h-[350px] object-cover rounded-[18px] bg-[#e8f8f6] p-3 shadow-[0_8px_20px_rgba(25,90,93,.12)]" />
         </div>
       </div>
     </section>
