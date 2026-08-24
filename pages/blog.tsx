a9da29f16fdc041745d51f6994d10b32cd2e1214
dentistcharan <dentist.charan@gmail.com>
2026-08-24 17:03:21 +0530

    new chnages add

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -5,9 +5,9 @@ import FAQSection from "@/components/FAQSection";
 import SitePage from "@/components/SitePage";
 import { getBlogsData, type BlogListData } from "@/lib/blogsApi";
 
-type Props = { data: BlogListData };
+type Props = { data: BlogListData; initialCategory: string };
 
-export default function BlogPage({ data }: Props) {
+export default function BlogPage({ data, initialCategory }: Props) {
   return (
     <SitePage
       title={data.pageSeo?.metaTitle || "Dental Blog | Elite Dental Studio"}
@@ -16,14 +16,21 @@ export default function BlogPage({ data }: Props) {
         "Read dental care tips, treatment guides and the latest news from Elite Dental Studio."
       }
     >
-      <BlogDirectory posts={data.items} />
+      <BlogDirectory posts={data.items} initialCategory={initialCategory} />
       <FAQSection />
       <BookAppointmentSection />
     </SitePage>
   );
 }
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({ res }) => {
+export const getServerSideProps: GetServerSideProps<Props> = async ({ query, res }) => {
   res.setHeader("Cache-Control", "public, s-maxage=60, stale-while-revalidate=300");
-  return { props: { data: await getBlogsData() } };
+  const data = await getBlogsData();
+  const requestedCategory = typeof query.category === "string" ? query.category : "All";
+  const initialCategory = data.items.some((post) =>
+    post.categories.some((category) => category.slug === requestedCategory),
+  )
+    ? requestedCategory
+    : "All";
+  return { props: { data, initialCategory } };
 };
